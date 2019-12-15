44d20a64cf65a6d02ffd7e24cbadfe11d45eebc7
WhiteOwl314 <parksj914@naver.com>
2019-12-16 03:22:07 +0900

    complete Date with interface

--- src/mydate/MyDate.java
+++ src/mydate/MyDate.java
@@ -1,4 +1,17 @@
 package mydate;
 
-public class MyDate {
+public class MyDate extends Date implements HandlingDate {
+    @Override
+    public void setDate(int year, int month, int day) {
+       setYear(year);
+       setMonth(month);
+       setDay(day);
+
+    }
+
+    @Override
+    public void showDescription() {
+
+        System.out.println("입력된 날짜: " + getYear() + "년 " + getMonth() + "월 " + getDay() + "일 입니다.");
+    }
 }
--- src/mydate/MyDateTest.java
+++ src/mydate/MyDateTest.java
@@ -4,6 +4,10 @@ public class MyDateTest {
 
     public static void main(String[] args) {
 
+        HandlingDate myDate = new MyDate();
 
-    }
+        myDate.setDate(2019, 12, 32);
+        myDate.showDescription();
+
+        }
 }
