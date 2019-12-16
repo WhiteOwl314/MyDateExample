d351f8adf89a6e7f03fbe72c29d4b70d8ddc38ce
WhiteOwl314 <parksj914@naver.com>
2019-12-16 15:10:23 +0900

    understand difference of int's address and String's addrsss

--- src/mydate/MyDate.java
+++ src/mydate/MyDate.java
@@ -1,6 +1,13 @@
 package mydate;
 
 public class MyDate extends Date implements HandlingDate {
+    public String getDate() {
+        if (getYear() != 0 && getMonth() != 0 && getDay() != 0) {
+            return (getYear() + "년 " + getMonth() + "월 " + getDay() + "일");
+        } else{
+            return "ERROR";
+        }
+    }
     @Override
     public void setDate(int year, int month, int day) {
        setYear(year);
@@ -13,5 +20,20 @@ public class MyDate extends Date implements HandlingDate {
     public void showDescription() {
 
         System.out.println("입력된 날짜: " + getYear() + "년 " + getMonth() + "월 " + getDay() + "일 입니다.");
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        MyDate objDown = (MyDate) obj;
+        MyDate thisDown = (MyDate) this;
+        if(getDate() != "ERROR") {
+            if(this.getDate() == ((MyDate) obj).getDate()){
+                return true;
+            } else {
+                return false;
+            }
+        } else{
+            return false;
+        }
     }
 }
--- src/mydate/MyDateTest.java
+++ src/mydate/MyDateTest.java
@@ -5,9 +5,21 @@ public class MyDateTest {
     public static void main(String[] args) {
 
         HandlingDate myDate = new MyDate();
+        HandlingDate myDate2 = new MyDate();
 
-        myDate.setDate(2019, 12, 32);
+        myDate.setDate(2019, 12, 31);
+        myDate2.setDate(2019,12,31);
         myDate.showDescription();
+
+        MyDate myDateDown = (MyDate) myDate;
+        MyDate mydate2Down = (MyDate) myDate2;
+        System.out.println(myDateDown.getDate());
+        System.out.println(((MyDate) myDate2).getDate());
+        System.out.println((myDateDown.getDate() == mydate2Down.getDate()));
+
+        System.out.println(myDate.equals(myDate2));
+
+        System.out.println(myDateDown.getDate());
 
         }
 }
